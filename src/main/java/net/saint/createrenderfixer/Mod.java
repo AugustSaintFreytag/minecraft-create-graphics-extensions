f044dfe28c39b9f9190e45583211b220faff91bd
August Saint Freytag <me@augustfreytag.com>
2025-12-25 11:42:12 +0100

    Set up DH integration into mod entry point

--- src/main/java/net/saint/createrenderfixer/Mod.java
+++ src/main/java/net/saint/createrenderfixer/Mod.java
@@ -3,7 +3,16 @@ package net.saint.createrenderfixer;
 import com.jozufozu.flywheel.api.instance.DynamicInstance;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.fabricmc.loader.api.FabricLoader;
 import net.saint.createrenderfixer.data.InstanceBlacklistManager;
+import net.saint.createrenderfixer.dh.ContraptionBlockRegistry;
+import net.saint.createrenderfixer.dh.ContraptionPersistencyUtil;
+import net.saint.createrenderfixer.dh.ContraptionRegistrationUtil;
+import net.saint.createrenderfixer.dh.DhBridge;
+import net.saint.createrenderfixer.dh.DhChunkProcessingHandler;
 import net.saint.createrenderfixer.utils.Logger;
 
 public class Mod implements ModInitializer {
@@ -23,15 +32,28 @@ public class Mod implements ModInitializer {
 	public void onInitialize() {
 		ModConfig.load();
 		ModCommands.init();
-	}
 
-	// Config
-
-	public static void reloadConfigProperties() {
-		synchronized (Mod.class) {
-			InstanceBlacklistManager.clear();
+		if (FabricLoader.getInstance().isModLoaded("distanthorizons")) {
+			initDistantHorizonsInterop();
 		}
 	}
+
+	private void initDistantHorizonsInterop() {
+		DhBridge.init();
+		DhChunkProcessingHandler.init();
+
+		ServerLifecycleEvents.SERVER_STARTED.register(ContraptionPersistencyUtil::load);
+		ServerLifecycleEvents.SERVER_STOPPING.register(ContraptionPersistencyUtil::save);
+
+		ServerWorldEvents.UNLOAD
+				.register((server, world) -> ContraptionBlockRegistry.clearForWorld(world.dimension().location().toString()));
+
+		ServerWorldEvents.LOAD.register((server, world) -> ContraptionRegistrationUtil.scanWorld(world));
+
+		ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> ContraptionRegistrationUtil.tryRegister(world, entity));
+	}
+
+	// Access
 
 	public static boolean isInstanceBlacklisted(DynamicInstance instance) {
 		return InstanceBlacklistManager.getCachedStatus(instance);
